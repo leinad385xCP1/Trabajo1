2b07e63b1d9b15c224c8483dea182e6c06087774
daniel <daniel.cardonag.col@upb.edu.co>
2023-08-19 12:35:49 -0500

    version2

--- Calculadora.java
+++ Calculadora.java
@@ -5,10 +5,14 @@ class Calculadora {
      int numero1;
      int numero2;
     double resultadoDivision;
-    
+    int i;
+
     System.out.println("bienvenido a calculadora");
+
+    Scanner scanner = new Scanner(System.in);
+do{
     
-     Scanner scanner = new Scanner(System.in);
+     
 
           System.out.println("ingresa numero 1");
     
@@ -18,14 +22,21 @@ class Calculadora {
     
      numero2 = scanner.nextInt();
      
-     scanner.close();
-
+     
     resultadoDivision = (double)numero1 /numero2;
     
      System.out.println("el resultado de la suma es " + (numero1+numero2));
      System.out.println("el resultado de la resta es " + (numero1-numero2));
      System.out.println("el resultado de la multiplicacion es " + (numero1*numero2));
      System.out.println("el resultado de la division es " + resultadoDivision);     
-    
-    }
-} 
+
+     System.out.println("desea continuar operando? si=1");
+
+        i= scanner.nextInt();
+
+}while(i==1);
+
+      scanner.close();
+  }
+
+}
